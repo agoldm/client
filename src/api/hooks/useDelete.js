ece323e24bcb2515ef47afdcd771d1ec2c8ccb2e
shiraDoron <shira27042000@gmail.com>
2022-08-03 12:36:51 +0300

    delete and add favorite course

--- src/api/hooks/useDelete.js
+++ src/api/hooks/useDelete.js
@@ -2,25 +2,25 @@ import React from "react";
 import http from '../http';
 
 // react hook to delete data from backend
-export default function useDelete(endpoint, body = null){
-        
-        const [data, setData] = React.useState(null);
-        const [loading, setLoading] = React.useState(true);
-        const [error, setError] = React.useState(false);
-        
-        const getData = async () => {
-            try {
-                const res = await http.delete(endpoint, body);
-                setData(res);
-                setLoading(false);
-            } catch (err) {
-                setError(true);
-                setLoading(false);
-            }  
-        }
+export default function useDelete(endpoint, body = null) {
 
-        React.useEffect(() => {
-                getData();
-            }, [endpoint]);
-        return { data, loading, error };
+    const [data, setData] = React.useState(null);
+    const [loading, setLoading] = React.useState(true);
+    const [error, setError] = React.useState(false);
+
+    const getData = async (body) => {
+        try {
+            const res = await http.delete(endpoint, body);
+            setData(res);
+            setLoading(false);
+        } catch (err) {
+            setError(true);
+            setLoading(false);
+        }
     }
+
+    React.useEffect(() => {
+        getData();
+    }, [endpoint]);
+    return { getData, data, loading, error };
+}
--- src/components/FavoriteCourses.js
+++ src/components/FavoriteCourses.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import useGet from '../api/hooks/useGet';
-
+import usePost from '../api/hooks/usePost';
 import Grid from '@mui/material/Grid';
 import { styled } from '@mui/material/styles';
 import { Box, Typography, Paper, Button, IconButton, Drawer } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
+import useDelete from '../api/hooks/useDelete';
+
 const Img = styled('img')({
     margin: 'auto',
     display: 'block',
@@ -16,17 +18,24 @@ const Img = styled('img')({
 function FavoriteCourses({ open, setOpen }) {
 
     const { data, loading, error } = useGet("users/favorite-course");
-
+    const signCourse = usePost("courses/signCourse");
+    const deleteFavoriteCourse = useDelete("users/favorite-course");
     if (loading) {
         return (<p>loading..</p>)
     }
     if (error) {
         return (<p>error..</p>)
     }
-
+    const courseAdd = async (courseId) => {
+        await signCourse.getData({ courseId: courseId });
+        await deleteFavoriteCourse.getData({ courseId: courseId });
+    }
+    const courseDelete = async (courseId) => {
+        await deleteFavoriteCourse.getData({ courseId: courseId });
+    }
     return (
         <Drawer anchor='right' open={open} sx={{ zIndex: 1500 }}>
-            <Box sx={{ width: '30vw',mt:7 }}>
+            <Box sx={{ width: '30vw', mt: 7 }}>
                 <IconButton onClick={() => setOpen(false)} sx={{ position: "absolute", top: 6, left: 6 }}>
                     <CloseIcon />
                 </IconButton>
@@ -40,12 +49,12 @@ function FavoriteCourses({ open, setOpen }) {
 
                                     </Typography>
                                     <Button color="primary" size="small"
-                                    // onClick={() => setLoginDialog(true)}
+                                    onClick={(e) => courseDelete(i._id)}
                                     >
                                         מחיקה
                                     </Button>
                                     <Button variant="contained" color="primary" size="small"
-                                    // onClick={() => setRegisterDialog(true)}
+                                        onClick={(e) => courseAdd(i._id)}
                                     >
                                         הרשמה
                                     </Button>
@@ -56,7 +65,7 @@ function FavoriteCourses({ open, setOpen }) {
 
                 ))}
             </Box>
-        </Drawer>
+        </Drawer >
     );
 }
 export default FavoriteCourses;
